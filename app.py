e39c51a1eb9928480ae009482788b2d300a17708
Mark McFadden <m2web@yahoo.com>
2023-08-28 20:14:13 -0400

    feat: :fre: Update app.py to real minimal function. 😊

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import os
 from apikey import apikey 
 import streamlit as st 
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain  
 
 # Set the OpenAI API key
 os.environ['OPENAI_API_KEY'] = apikey
@@ -15,17 +13,8 @@ st.title('🦜🔗 Mark\'s GPT Creator')
 # Create a text input for the user to enter their prompt
 prompt = st.text_input('Plug in your prompt here') 
 
-# Define a template for generating YouTube video titles
-# title_template = PromptTemplate(
-#    input_variables = ['topic'], 
-#    template='write me a youtube video title about {topic}'
-#)
-
 # Initialize an OpenAI language model with a temperature of 0.9
 llm = OpenAI(temperature=0.9) 
-
-# Initialize a language model chain with the title template
-# title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
 
 # If the user has entered a prompt, generate a response using the language model
 if prompt: 

cbe095e7b42249db73da4de0e64d499c4c24f0c7
Mark McFadden <m2web@yahoo.com>
2023-09-18 19:39:41 -0400

    feat: :fire: Update app.py with app-talk-like-a-pirate code.

--- app.py
+++ app.py
@@ -1,58 +1,41 @@
+# Import necessary modules
 import os 
 from apikey import apikey 
-
 import streamlit as st 
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain 
-from langchain.memory import ConversationBufferMemory
-from langchain.utilities import WikipediaAPIWrapper 
 
 # Set the OpenAI API key
 os.environ['OPENAI_API_KEY'] = apikey
 
-# Set the OpenAI API key
-st.title('🦜🔗 YouTube GPT Creator')
-prompt = st.text_input('Plug in your prompt here') 
+# Set the title of the Streamlit app
+st.title('🦜🔗 Mark\'s GPT Creator')
 
-# Define prompt templates for generating video titles and scripts
-title_template = PromptTemplate(
-    input_variables = ['topic'], 
-    template='write me a youtube video title about {topic}'
-)
+def generate_response(llm, prompt):
+    """
+    Generates a response using the specified language model and prompt.
+    """
+    response = llm(prompt)
+    # clear the prompt textbox
+    st.text_input('Enter your prompt here:', '')
+    st.write(response)
 
-script_template = PromptTemplate(
-    input_variables = ['title', 'wikipedia_research'], 
-    template='write me a youtube video script based on this title TITLE: {title} while leveraging this wikipedia reserch:{wikipedia_research} '
-)
-
-# Set up memory buffers for storing previous conversations
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
-script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-
-
-# Set up the OpenAI language model and LLM chains for generating titles and scripts
+# Initialize an OpenAI language model with a temperature of 0.9
 llm = OpenAI(temperature=0.9) 
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
 
-# Set up a Wikipedia API wrapper for retrieving research information
-wiki = WikipediaAPIWrapper()
+# Create a text input for the user to enter their prompt 
+prompt = """It's talke like a pirate day so please respond as a priate would talk. Here are some
+examples:
 
-# Show the prompt and generated content on the screen if there is a prompt
+User: How are you?
+AI: Aarrrgg! Maties!! I'm great and talking like a pirate!
+
+User: What time is it?
+AI: Arrgg! IT's time to sail the 7 seas!
+"""
+
+# use the above prompt with user input
+prompt = st.text_input('Enter your prompt here:', prompt)
+
+# If the user has entered a prompt, generate a response using the language model
 if prompt: 
-    title = title_chain.run(prompt)
-    wiki_research = wiki.run(prompt) 
-    script = script_chain.run(title=title, wikipedia_research=wiki_research)
-
-    st.write(title) 
-    st.write(script) 
-
-    with st.expander('Title History'): 
-        st.info(title_memory.buffer)
-
-    with st.expander('Script History'): 
-        st.info(script_memory.buffer)
-
-    with st.expander('Wikipedia Research'): 
-        st.info(wiki_research)
+    generate_response(llm, prompt)

13e01fd7561146e9952dadced0fdd79087c3a881
Mark McFadden <m2web@yahoo.com>
2023-09-19 06:29:36 -0400

    feat: :fire: Update app-talk-like-a-pirate.py file prompt and variable names.

--- app-talk-like-a-pirate.py
+++ app-talk-like-a-pirate.py
@@ -1,3 +1,6 @@
+# Description: A Streamlit app that uses the OpenAI API to generate a reponse that talks like a pirate.
+
+# Import the required libraries
 import os 
 from apikey import apikey 
 
@@ -11,44 +14,45 @@ from langchain.utilities import WikipediaAPIWrapper
 # Set the OpenAI API key
 os.environ['OPENAI_API_KEY'] = apikey
 
-# Set the OpenAI API key
+# Set the title of the app
 st.title('🦜🔗 Mark\'s Pirate ChatGPT')
-prompt = st.text_input('Plug in your prompt here') 
 
-# Define prompt templates for generating video titles and scripts
-title_template = PromptTemplate(
-    input_variables = ['topic'], 
-    template='talk like a pirate about {topic}'
+# Set the prompt for the user to enter
+prompt = st.text_input('Your Prompt Me Hearties:', placeholder='Enter your words here, Savvy?') 
+
+# Define prompt template for generating responses like a pirate
+subject_template = PromptTemplate(
+    input_variables = ['subject'], 
+    template='talk like a pirate about {subject}'
 )
 
+# Define prompt template for generating discussions like a pirate
 script_template = PromptTemplate(
     input_variables = ['title'], 
     template='discuss like a pirate {title}'
 )
 
 # Set up memory buffers for storing previous conversations
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+subject_memory = ConversationBufferMemory(input_key='subject', memory_key='chat_history')
 script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 
 
 # Set up the OpenAI language model and LLM chains for generating titles and scripts
 llm = OpenAI(temperature=0.9) 
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+subject_chain = LLMChain(llm=llm, prompt=subject_template, verbose=True, output_key='title', memory=subject_memory)
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
-
-# Set up a Wikipedia API wrapper for retrieving research information
-wiki = WikipediaAPIWrapper()
 
 # Show the prompt and generated content on the screen if there is a prompt
 if prompt: 
-    title = title_chain.run(prompt)
+    title = subject_chain.run(prompt)
     script = script_chain.run(title=title)
 
+    # Show the title and script on the screen
     st.write(title) 
     st.write(script) 
-
-    with st.expander('Title History'): 
-        st.info(title_memory.buffer)
-
+    
+    # Show the subject and script history in an expander
+    with st.expander('Subject History'): 
+        st.info(subject_memory.buffer)
     with st.expander('Script History'): 
         st.info(script_memory.buffer)
